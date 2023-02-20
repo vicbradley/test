618d7ad7aa205ff1a31cb4f0394c15bde13dcbb9
Vic Bradley <vicbradley33@gmail.com>
2023-02-20 15:36:51 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import MarkdownPreviewer from "./components/MarkdownPreviewer.js";
+import "./css/style.scss"
 
 
 function App() {
-    return <MarkdownPreviewer />;
+    // return <MarkdownPreviewer />;
+    return (
+        <h2>hello world</h2>
+    )
 }
 
 export default App;
